b2f96eb444c3e13fb94c8a91a926773cf4010a7a
Giang Phan <john.phan@gmail.com>
2017-07-09 15:14:06 -0700

    Adding axios dependency and correctly running build in postbuild

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/api', proxy({
   target: 'https://snkrs.prod.commerce.nikecloud.com',
@@ -14,7 +14,7 @@ app.use('/api', proxy({
 }));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/build/index.html'));
+  res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 
 const port = process.env.PORT || 3000;
